ff27dd7842cbfb386e0f0347e226fa57ceeea871
lindaayangg <lindayang0928@hotmail.com>
2019-11-03 00:54:06 -0500

    Add About section and card component

---
+++ src/components/InfoCard/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import {CardDescription} from "semantic-ui-react";
+
+export const StyledCardDescription = styled(CardDescription)`
+  &&& {
+    line-height: 1.8rem;
+    font-size: 15px;
+  }
+`;
