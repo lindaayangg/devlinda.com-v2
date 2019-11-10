de317600d64f99544640df6908a06b59431d7b11
lindaayangg <lindayang0928@hotmail.com>
2019-11-09 22:19:34 -0600

    Fix margins

--- src/sections/Experience/styles.js
+++ src/sections/Experience/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledExperienceContainer = styled.div`
   &&& {
     width: 83%;
-    margin-top: 10px;
+    margin-top: 50px;
     margin-bottom: 30px;
   }
 `;
